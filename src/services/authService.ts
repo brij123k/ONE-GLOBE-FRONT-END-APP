a0672e58afc97dc9e460a5b1887a69a72c9475a3
Abhishek Maurya <abhishek639398maurya@gmail.com>
2026-02-24 19:46:26 +0530

    added some changes

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BASE_URL } from './apiConfig';
+import { BASE_URL } from '@/services/apiConfig';
 
 export async function loginShop(shop: string) {
   const response = await axios.post(`${BASE_URL}/api/auth/login`, {
