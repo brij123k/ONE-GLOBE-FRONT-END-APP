4cfc5a4de2841037fb0fcfb052e0a292d515f47c
Abhishek Maurya <abhishek639398maurya@gmail.com>
2026-02-24 19:50:20 +0530

    added some changes

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:3000";
-const BASE_URL = "https://starfish-app-djdfs.ondigitalocean.app"
+export const BASE_URL = "https://starfish-app-djdfs.ondigitalocean.app"
 const ApiConfig = {
   baseUrl: BASE_URL,
   // shopify products
