d28285e6ed84ac83738c4d48e1a4edba31ad9016
Abhishek Maurya <abhishek639398maurya@gmail.com>
2026-02-28 14:25:40 +0530

    added some changes

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useLocation } from "react-router-dom";
 import {
   LayoutDashboard,
   Type,
